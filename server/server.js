10ae599955f55a6a649aba5d2f97e51d2bdd0c0e
Mitch Alderson <mitchell@apollographql.com>
2022-09-12 10:55:31 -0700

    suggestions

--- server/server.js
+++ server/server.js
@@ -1,104 +1,109 @@
-const { ApolloServer, gql, AuthenticationError } = require('apollo-server');
+const { ApolloServer, gql, AuthenticationError } = require("apollo-server");
 
-member_list = [{
-    "id": "1",
-    "name": "Shane"
-},
-{
-    "id": "2",
-    "name": "Kyle"
-},
-{
-    "id": "3",
-    "name": "Mitch"
-},
-{
-    "id": "4",
-    "name": "Mario"
-},
-{
-    "id": "5",
-    "name": "Gaylen"
-}];
+const member_list = [
+  {
+    id: "1",
+    name: "Shane",
+  },
+  {
+    id: "2",
+    name: "Kyle",
+  },
+  {
+    id: "3",
+    name: "Mitch",
+  },
+  {
+    id: "4",
+    name: "Mario",
+  },
+  {
+    id: "5",
+    name: "Gaylen",
+  },
+];
 
-location_list = [{
-    "id": "1",
-    "name": "Tampa"
-},
-{
-    "id": "2",
-    "name": "Austin"
-},
-{
-    "id": "3",
-    "name": "Bay Area"
-},
-{
-    "id": "4",
-    "name": "Bay Area"
-},
-{
-    "id": "5",
-    "name": "Austin"
-}];
+const location_list = [
+  {
+    id: "1",
+    name: "Tampa",
+  },
+  {
+    id: "2",
+    name: "Austin",
+  },
+  {
+    id: "3",
+    name: "Bay Area",
+  },
+  {
+    id: "4",
+    name: "Bay Area",
+  },
+  {
+    id: "5",
+    name: "Austin",
+  },
+];
 
 const typeDefs = gql`
-    
-    type Query {
-        Member: [Member]
-        Location: [Location]
-    }
+  type Query {
+    Member: [Member]
+    Location: [Location]
+  }
 
-    type Member {
-        id: ID!
-        name: String!
-    }
+  type Member {
+    id: ID!
+    name: String!
 
-    type Location {
-        id: ID!
-        name: String!
-    }
+    # Should have a field resolver that matches up the member to the location
+    location: Location!
+  }
+
+  type Location {
+    id: ID!
+    name: String!
+  }
 `;
 
-
 const resolvers = {
-    Query: {
-        Member: (_,{name}, {dataSources, token}) => {
-            //Resolver level auth null check
-             if (!token) throw new AuthenticationError("auth error");
-            return dataSources.members},
-        Location: (_,{name}, {dataSources, token}) => {
-            //Resolver level auth nullcheck
-            if (!token) throw new AuthenticationError("auth error");
-            return dataSources.locations}
-    }
+  Query: {
+    Member: (_, { name }, { dataSources, token }) => {
+      //Resolver level auth null check
+      if (!token) throw new AuthenticationError("auth error");
+      return dataSources.members;
+    },
+    Location: (_, { name }, { dataSources, token }) => {
+      //Resolver level auth nullcheck
+      if (!token) throw new AuthenticationError("auth error");
+      return dataSources.locations;
+    },
+  },
 };
 
-
 const server = new ApolloServer({
-    typeDefs, 
-    resolvers,
-    csrfPrevention: true,
-    cache: 'bounded',
-    context: async ({ req }) => {
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  context: async ({ req }) => {
+    //Extract token from header
+    const token = req.headers.authentication || "";
 
-        //Extract token from header
-        const token = req.headers.authorization || '';
+    //Check if user ID is null
+    if (!token) throw new AuthenticationError("account error");
 
-        //Check if user ID is null
-        if (!token) throw new AuthenticationError("account error");
-
-        //return token
-        return { token };
-
-    },
-    dataSources: () => {
-        return {
-          locations: location_list,
-          members: member_list,
-        };
-      },
+    //return token
+    return { token };
+  },
+  dataSources: () => {
+    return {
+      locations: location_list,
+      members: member_list,
+    };
+  },
 });
 
-server.listen({port: 9000})
-    .then(({url}) => console.log(`Server running at ${url}`))
+server
+  .listen({ port: 9000 })
+  .then(({ url }) => console.log(`Server running at ${url}`));
--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, gql, HttpLink } from '@apollo/client';
-import './App.css';
-import Homepage from './pages/Homepage';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  HttpLink,
+} from "@apollo/client";
+import "./App.css";
+import Homepage from "./pages/Homepage";
 
 function App() {
-
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: 'http://localhost:9000/',
-  })
+    uri: "http://localhost:9000/",
+    headers: {
+      // test auth header
+      authentication: "test",
+    },
+  });
   return (
     <ApolloProvider client={client}>
       <Homepage />
