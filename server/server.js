fddedbbeb17a107b2de5f2d886cfae939c960c31
Mitch Alderson <mitchell@apollographql.com>
2022-10-13 12:37:34 -0700

    fix resolvers

--- server/server.js
+++ server/server.js
@@ -1,108 +1,116 @@
-const { argumentsObjectFromField } = require('@apollo/client/utilities');
-const { ApolloServer, gql, AuthenticationError } = require('apollo-server');
+const { argumentsObjectFromField } = require("@apollo/client/utilities");
+const { ApolloServer, gql, AuthenticationError } = require("apollo-server");
 
-member_list = [{
-    "id": "1",
-    "name": "Shane"
-},
-{
-    "id": "2",
-    "name": "Kyle"
-},
-{
-    "id": "3",
-    "name": "Mitch"
-},
-{
-    "id": "4",
-    "name": "Mario"
-},
-{
-    "id": "5",
-    "name": "Gaylen"
-}];
+const member_list = [
+  {
+    id: "1",
+    name: "Shane",
+  },
+  {
+    id: "2",
+    name: "Kyle",
+  },
+  {
+    id: "3",
+    name: "Mitch",
+  },
+  {
+    id: "4",
+    name: "Mario",
+  },
+  {
+    id: "5",
+    name: "Gaylen",
+  },
+];
 
-location_list = [{
-    "id": "1",
-    "name": "Tampa"
-},
-{
-    "id": "2",
-    "name": "Austin"
-},
-{
-    "id": "3",
-    "name": "Phoenix"
-},
-{
-    "id": "4",
-    "name": "Vancouver"
-},
-{
-    "id": "5",
-    "name": "Austin"
-}];
+const location_list = [
+  {
+    id: "1",
+    name: "Tampa",
+  },
+  {
+    id: "2",
+    name: "Austin",
+  },
+  {
+    id: "3",
+    name: "Phoenix",
+  },
+  {
+    id: "4",
+    name: "Vancouver",
+  },
+  {
+    id: "5",
+    name: "Austin",
+  },
+];
 
 const typeDefs = gql`
-    
-    type Query {
-        Members: [Member]
-        Location(id: ID!): [Location]
-    }
+  type Query {
+    Members: [Member]
+    Location: [Location]
+  }
 
-    "look into including Location as field"
-    type Member {
-        id: ID!
-        name: String!
-        location(id: ID!): Location!
-    }
+  "look into including Location as field"
+  type Member {
+    id: ID!
+    name: String!
+    location: Location!
+  }
 
-    type Location {
-        id: ID!
-        name: String!
-    }
+  type Location {
+    id: ID!
+    name: String!
+  }
 `;
 
-
 const resolvers = {
-    Query: {
-        Members: (_,{name}, {dataSources, token}) => {
-            //Resolver level auth null check
-            if (token == null) throw new AuthenticationError("auth error");
-            return dataSources.members},
-        Location: (parent, args, {dataSources, token}) => {
-            //Resolver level auth nullcheck
-            if (token == null) throw new AuthenticationError("auth error");
-            return dataSources.locations}
-            //return dataSources.locations.find(location => location.id === args.id)}
-    }
+  Query: {
+    Members: (_, { name }, { dataSources, token }) => {
+      //Resolver level auth null check
+      if (token == null) throw new AuthenticationError("auth error");
+      return dataSources.members;
+    },
+    Location: (parent, args, { dataSources, token }) => {
+      //Resolver level auth nullcheck
+      if (token == null) throw new AuthenticationError("auth error");
+      return dataSources.locations;
+    },
+  },
+  Member: {
+    location: (parent, args, { dataSources, token }) => {
+      return dataSources.locations.find(
+        (location) => location.id === parent.id
+      );
+    },
+  },
 };
 
-
 const server = new ApolloServer({
-    typeDefs, 
-    resolvers,
-    csrfPrevention: true,
-    cache: 'bounded',
-    context: async ({ req }) => {
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  context: async ({ req }) => {
+    //Extract token from header
+    const token = req.headers.authorization || "";
 
-        //Extract token from header
-        const token = req.headers.authorization || '';
+    //Check if user ID is null
+    if (token == null) throw new AuthenticationError("account error");
 
-        //Check if user ID is null
-        if (token == null) throw new AuthenticationError("account error");
-
-        //return token
-        return { token };
-
-    },
-    dataSources: () => {
-        return {
-          locations: location_list,
-          members: member_list,
-        };
-      },
+    //return token
+    return { token };
+  },
+  dataSources: () => {
+    return {
+      locations: location_list,
+      members: member_list,
+    };
+  },
 });
 
-server.listen({port: 9000})
-    .then(({url}) => console.log(`Server running at ${url}`))
+server
+  .listen({ port: 9000 })
+  .then(({ url }) => console.log(`Server running at ${url}`));
