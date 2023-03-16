98ab90800ab92ccaa8b00b67f3bb5586530ae003
Shane Coll <shanewilliamcoll@gmail.com>
2023-03-16 15:04:40 -0400

    resovers working

--- server/subgraph-members/resolvers.js
+++ server/subgraph-members/resolvers.js
@@ -3,19 +3,13 @@ const { token } = require('./index.js')
 
 const resolvers = {
     Query: {
-        Member: (_,{name}, {dataSources, token}) => {
+        Members: (_,{name}, {dataSources, token}) => {
             console.log("Member Query");
             //Resolver level auth null check
             if (token == null) throw new AuthenticationError("auth error");
-            return dataSources.members},
+            return dataSources.members;
+        },
     },
-    /*Member: {
-        __resolveReference: ({locationId}, {dataSources}) => {
-            console.log("Hello!!!");
-            const locations = dataSources.find(l => l.locationId === locationId);
-            return locations.locationId;
-          },
-    },*/
     Location: {
         __resolveReference: ({locationId}) => {
             console.log("location resolveref");
@@ -24,18 +18,10 @@ const resolvers = {
         },
         membersInLocation: (parent, {name}, {dataSources, token}) => {
             console.log("membersInLocation")
-            const locations = dataSources.members.find(l => l.locationId === parent.id);
+            const locations = dataSources.members.filter(l => l.locationId === parent.id);
             return locations;
         },
-    }
-        /*__resolveReference: ({ locationId }, { dataSources }) => {
-          console.log(locationId);
-          console.log("A");
-          const locations = dataSources.find(l => l.locationId === locationId);
-          return locations;
-        },
-        Member: (parent, _, )*/
-      
+    },
 };
 
 module.exports = resolvers;
