a4e5bff2513b8cea703f4c6d8b1854d1729b62ca
Shane Coll <shanewilliamcoll@gmail.com>
2023-03-16 15:04:05 -0400

    reference resolvers working

--- server/subgraph-locations/resolvers.js
+++ server/subgraph-locations/resolvers.js
@@ -1,18 +1,13 @@
 const resolvers = {
     Query: {
-        Location: (_,{name}, {dataSources, token}) => {
+        Locations: (_,{name}, {dataSources, token}) => {
             //Resolver level auth nullcheck
             console.log("Query location - location subgraph");
             if (token == null) throw new AuthenticationError("auth error");
             //console.log(dataSources.locations);
-            return dataSources.locations}
-    },
-    Location: {
-        __resolveReference: ({locationId}) => {
-            console.log("location ref resolve-location subgraph");
-            return {id: locationId};
+            return dataSources.locations;
         },
-    }
+    },
 };
   
   module.exports = resolvers;
